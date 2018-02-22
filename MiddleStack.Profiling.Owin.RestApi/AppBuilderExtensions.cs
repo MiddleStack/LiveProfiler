39aaad67f8e280a1d08f8536421c8a0caf6d29ce
Tony Chow <tony@bluetentacle.com>
2018-02-21 16:27:04 -0800

    Moving the REST API to a more specific base path.

--- MiddleStack.Profiling.Owin.RestApi/AppBuilderExtensions.cs
+++ MiddleStack.Profiling.Owin.RestApi/AppBuilderExtensions.cs
@@ -11,21 +11,25 @@ namespace MiddleStack.Profiling.Owin.RestApi
         /// <remarks>
         ///     <para>The endpoints offered are:</para>
         ///     <list type="bullet">
-        ///         <item>GET /v1/transactions/recent</item>
-        ///         <item>GET /v1/transactions/inflight</item>
+        ///         <item>GET /liveprofiler/api/v1/transactions/recent</item>
+        ///         <item>GET /liveprofiler/api/v1/transactions/inflight</item>
         ///     </list>
         /// </remarks>
         /// <param name="appBuilder">
         ///     The <see cref="IAppBuilder"/> to which to add the LiveProfiler API endpoints.
         /// </param>
+        /// <param name="basePath">
+        ///     The starting portion of the path to the API endpoints. By default, it's /liveprofiler/api.
+        ///     But users can override this to customize routing.
+        /// </param>
         /// <exception cref="ArgumentNullException">
         ///     The argument <paramref name="appBuilder"/> is <see langword="null"/>.
         /// </exception>
-        public static void UseLiveProfilerRestApi(this IAppBuilder appBuilder)
+        public static void UseLiveProfilerRestApi(this IAppBuilder appBuilder, string basePath = "/liveprofiler/api")
         {
             if(appBuilder == null) throw new ArgumentNullException(nameof(appBuilder));
 
-            appBuilder.Use<LiveProfilerRestApiMiddleware>();
+            appBuilder.Use<LiveProfilerRestApiMiddleware>(basePath);
         }
     }
 }
